b0c16cd57369269be3cb6988cff553d988c21b20
Lynda Guillu <lynda_guillu@att.net>
2017-02-28 12:50:40 -0600

    Added String Interpolation

--- store_item.rb
+++ store_item.rb
@@ -1,13 +1,61 @@
-#Using Hashes
+# #Using Hashes
 
-car1 = {"car" => "VW", "year" => "2012", "color" => "red"}
-car2 = {"car" => "jeep", "year" => "2010", "color" => "blue"}
-car3 = {"car" => "karma Gia", "year" => "1966", "color" => "yellow"}
+employee1 = ["Daniela", "Gallardo", 60000]
+employee2 = ["Michael", "Jobs", 76000]
+employee3 = ["Phil", "Skippy", 99000]
 
-puts " Car one is #{car1["car"]}  #{car1["year"]} and the color is #{car1["color"]}"
-puts " Car two is #{car2["car"]}  #{car2["year"]} and the color is #{car2["color"]}"
-puts " Car three is #{car3["car"]}  #{car3["year"]} and the color is #{car3["color"]}"
+#String Interpolation
+   puts "#{employee1[0]}  #{employee1[1]} makes $#{employee1[2]} a year"
+   puts "#{employee2[0]}  #{employee2[1]} makes $#{employee2[2]} a year"
+   puts "#{employee3[0]}  #{employee3[1]} makes $#{employee3[2]} a year"
+
+
+
 puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 puts
-# Using Hashes with symbols
-puts " Car three is #{car1[:car]}  "
+# # Using Hashes with symbols
+# puts " Car three is #{employee1[:car]}  "
+
+# class Car
+
+#   def initialize(input_car, input_year, input_color)
+#     @car = input_car
+#     @year = input_year
+#     @color = input_color
+#   end
+
+#   def print_info
+#     puts "#{@car} #{@year} #{@color}"
+#   end
+
+# end
+
+
+#   employee1 = Car.new("BMw", "2017", "Black")
+#   # car2 = Car.new("car", "year", "color")
+#   # car3 = Car.new("car", "year", "color")
+# puts employee1.print_info
+
+
+# Change Initialize to a HASH
+
+# class Car
+
+#   def initialize(input_options)
+#     @car = input_options[:car]
+#     @year = input_options[:year]
+#     @color = input_options[:color]
+#   end
+
+#   def print_info
+#     puts "#{@car} #{@year} #{@color}"
+#   end
+
+# end
+
+
+#   employee1 = Car.new("BMW", "2017", "Black")
+#   # car2 = Car.new("car", "year", "color")
+#   # car3 = Car.new("car", "year", "color")
+# puts employee1.print_info
+
