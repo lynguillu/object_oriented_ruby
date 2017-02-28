52db2fdce18be8b18a02b202e6dc831d01154df9
Lynda Guillu <lynda_guillu@att.net>
2017-02-27 20:32:28 -0600

    added hashes with symbols

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
+#Using Hashes
+
 car1 = {"car" => "VW", "year" => "2012", "color" => "red"}
 car2 = {"car" => "jeep", "year" => "2010", "color" => "blue"}
 car3 = {"car" => "karma Gia", "year" => "1966", "color" => "yellow"}
@@ -5,3 +7,7 @@ car3 = {"car" => "karma Gia", "year" => "1966", "color" => "yellow"}
 puts " Car one is #{car1["car"]}  #{car1["year"]} and the color is #{car1["color"]}"
 puts " Car two is #{car2["car"]}  #{car2["year"]} and the color is #{car2["color"]}"
 puts " Car three is #{car3["car"]}  #{car3["year"]} and the color is #{car3["color"]}"
+puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+puts
+# Using Hashes with symbols
+puts " Car three is #{car1[:car]}  "
